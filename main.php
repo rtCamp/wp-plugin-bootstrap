08d3baadfa51257eab8aef1c6b16eb0d72dfbfb3
Rahul Bansal <rahul.bansal@rtcamp.com>
2012-10-06 16:05:28 +0530

    first commit

---
+++ main.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: Plugin Name
+Plugin URI: http://example.com/plugin-page/
+Description: Long description upto 140 chars (need to check exact limit) 
+Version: 0.1 
+Author: rtCamp
+Author URI: http://example.com/
+Requires at least: 3.0
+Tested up to: 3.4.2
+*/
+
+/** You may noe start coding! **/
+
+?>
