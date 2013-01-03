23e3fe3e6a88450c83e13d608c17047038004f7e
Mitesh Shah <Mr.Miteshah@gmail.com>
2013-01-03 14:41:05 +0530

    Common Deploy Script

--- deploy-common.sh
+++ deploy-common.sh
@@ -59,6 +59,7 @@ git checkout-index -a -f --prefix=$SVNPATH/trunk/
 
 echo "Ignoring github specific files and deployment script"
 svn propset svn:ignore "deploy.sh
+deploy-common.sh
 readme.sh
 README.md
 .git
